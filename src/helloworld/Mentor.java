e223c121e01e5395d748dc63dba89d53c2e6b6c9
kdaud <dkakumirizii@gmail.com>
2020-01-24 16:01:57 +0300

    files

---
+++ src/helloworld/Mentor.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package helloworld;
+
+import java.util.Random;
+
+/**
+ * @author admins
+ *
+ */
+public class Mentor {
+	private static String  name;
+	
+	//private Date mybirthday;
+	//private Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		System.out.println("John speak");
+		
+		System.out.println(toString("Google Code-in 2019 with OpenMRS"));
+		System.out.println(myNumber());
+	}
+	public static String toString(String message){
+		
+			return "We want to say thank you for your great work in "+message;
+	}
+	
+	private static int myNumber() {
+		Random rad = new Random();
+		System.out.println("Success!");
+		int num = rad.nextInt(87);
+		if (num % 2 == 0) {
+			System.out.println("Its a multiple of 2");
+		}
+		return num;
+	}
+
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author admins
+ *
+ */
+module helloworld {
+}
